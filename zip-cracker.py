a8e1d643579683be89d21f6cfa2e6e83bca71017
broadbentt <ghostagent@btinternet.com>
2019-09-07 18:46:46 +0100

    changes

--- zip-cracker.py
+++ zip-cracker.py
@@ -2,26 +2,25 @@
 # coding:UTF-8
 
 # -------------------------------------------------------------------------------------
-#                  PYTHON UTILITY FILE TO CRACK ENCRYPTED ZIP FILES
+#                  A PYTHON SCRIPT FILE TO CRACK ENCRYPTED ZIP FILES
 #                BY TERENCE BROADBENT BSc CYBER SECURITY (FIRST CLASS)
 # -------------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Load required imports.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
 import os
 import sys
-import fileinput
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Conduct simple and routine tests on supplied arguements.   
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
@@ -46,12 +45,30 @@ if filextends != "zip":
    print "Incorrect file format...."
    exit (True)
 
-filename = "'" + filename + "'"
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 1.0                                                                
+# Details : Create function call for my header display.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------
+
+def header ():
+   os.system("clear")
+   print " ________ ____     ____ ____      _    ____ _  _______ ____   "
+   print "|__  /_ _|  _ \   / ___|  _ \    / \  / ___| |/ / ____|  _ \  "
+   print "  / / | || |_) | | |   | |_) |  / _ \| |   | ' /|  _| | |_) | "
+   print " / /_ | ||  __/  | |___|  _ <  / ___ \ |___| . \| |___|  _ <  "
+   print "/____|___|_|      \____|_| \_\/_/   \_\____|_|\_\_____|_| \_\ "
+   print "                                                              "
+   print "    BY TERENCE BROADBENT BSc CYBER SECURITY (FIRST CLASS)     "
+   print "                                                              "
+   print "FILENAME: " + filename + ".\n"
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Check all required dependencies are installed on the system.
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
@@ -59,7 +76,7 @@ filename = "'" + filename + "'"
 checklist = ["rockyou", "fcrackzip", "hashcat"]
 installed = True
 
-print ""
+header()
 for check in checklist:
    cmd = "locate -i " + check + " > /dev/null"
    checked = os.system(cmd)
@@ -74,27 +91,7 @@ if installed == False:
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
-# Details : Display my universal banner.
-# Modified: N/A                                                               
-# -------------------------------------------------------------------------------------
-
-def header ():
-   os.system("clear")
-   print " ________ ____     ____ ____      _    ____ _  _______ __     "
-   print "|__  /_ _|  _ \   / ___|  _ \    / \  / ___| |/ / ____|  _ \  "
-   print "  / / | || |_) | | |   | |_) |  / _ \| |   | ' /|  _| | |_) | "
-   print " / /_ | ||  __/  | |___|  _ <  / ___ \ |___| . \| |___|  _ <  "
-   print "/____|___|_|      \____|_| \_\/_/   \_\____|_|\_\_____|_| \_\ "
-   print "                                                              "
-   print "    BY TERENCE BROADBENT BSc CYBER SECURITY (FIRST CLASS)     "
-   print "                                                              "
-   print "FILENAME: " + filename + "\n"
-
-# -------------------------------------------------------------------------------------
-# AUTHOR  : Terence Broadbent                                                    
-# CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : The main menu system.
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
@@ -117,21 +114,21 @@ while True:
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Menu option selected - Dictionary attack.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
    if selection =='1':
       print "Crack Selected    : Dictionary attack."
-      dictionary = "/usr/share/wordlists/rockyou.txt"		# change as required
+      dictionary = "/usr/share/wordlists/rockyou.txt"		# change as you require.
       if os.path.isfile(dictionary):
          print "Crack Dictionary  : " + dictionary + "."
       else:
          print "System Error      : Dictionary not found."
          exit(True)
       print "Crack Status      : Cracking file."
-      os.system("fcrackzip -v -D -u -p " + dictionary + " " + filename + " > Answer.txt")
+      os.system("fcrackzip -v -D -u -p " + dictionary + " " + "'" + filename + "'" + " > Answer.txt")
       os.system("awk '/pw ==/{print $NF}' Answer.txt > Password.txt")
       password = open("Password.txt").readline().rstrip()
       if password == "":	
@@ -145,15 +142,15 @@ while True:
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Menu option selected - Hash attack.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-   if selection == '2':
+   elif selection == '2':
       print "Crack Selected    : Hash attack."
       print "Crack Status      : Creating hash."
-      os.system("zip2john " + filename + " > hash.txt 2>&1")
+      os.system("zip2john " + "'" + filename + "'" + " > hash.txt 2>&1")
       os.system("sed 1d hash.txt > hash2.txt")
       os.remove('hash.txt')
       print "Crack Status      : Comparing hash values."
@@ -173,15 +170,15 @@ while True:
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Menu option selected - Brute force attack.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-   if selection == '3':
+   elif selection == '3':
       print "Crack Selected    : Brute force attack (1-8 characters)."
       print "Crack Status      : Conducting numeric attack first."
-      os.system("fcrackzip -c 1 -m zip1 -l 1-8 -u " + filename + " > Answer.txt")
+      os.system("fcrackzip -c 1 -m zip1 -l 1-8 -u " + "'" + filename + "'" + " > Answer.txt")
       os.system("awk '/pw ==/{print $NF}' Answer.txt > Password.txt")
       password = open("Password.txt").readline().rstrip()
       if password == "":	
@@ -190,7 +187,7 @@ while True:
          print "Cracked Password  : " + password + ".\n"
       if password == "":
          print "Crack Status      : Now trying alphanumeric."
-         os.system("fcrackzip -m zip1 -l 1-8 -u " + filename + " > Answer.txt")
+         os.system("fcrackzip -m zip1 -l 1-8 -u " + "'" + filename + "'" + " > Answer.txt")
          os.system("awk '/pw ==/{print $NF}' Answer.txt > Password.txt")
          password = open("Password.txt").readline().rstrip()
          if password == "":	
@@ -204,11 +201,11 @@ while True:
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 1.1                                                                
+# Version : 1.0                                                                
 # Details : Menu option selected - Quit program.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-   if selection == '4': 
+   elif selection == '4': 
       exit(False)
 #Eof
