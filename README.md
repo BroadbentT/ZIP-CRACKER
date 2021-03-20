76562a45895c3c704387a938843d3ef54530d0a6
root <root@kali.Domain>
2021-03-20 17:27:10 +0000

    Python3 Update

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ZIP CRACKER
 ## A PYTHON SCRIPT FILE TO CRACK ENCRYPTED .ZIP FILES USING A DICTIONARY ATTACK, A HASH ATTACK, OR A ALPHANUMERIC BRUTE FORCE ATTACK.
 
-Usage: python zip-cracker.py secure.zip
+Usage: python3 zip-cracker.py secure.zip
 
 | LANGUAGE | FILENAME       | MD5 HASH                         | CONTENTS      |
 |--------  |---------       |---------                         |---------      |
-| python   | zip-cracker.py | bb2b83f030c9cb36e90b2b48a75c095d |               | 
+| python3  | zip-cracker.py | bac267051733ea42584e288a88f7a7ea |               | 
 | zip      | secure.zip     | 35ebcf246fbab4c7106dde9e0c57d8e4 | topsecret.jpg |
 
 The script is menu driven and allows the user to choose between a dictionary attack, a hash attack, or a alphanumeric brute force attack on the specified file.
--- zip-cracker.py
+++ zip-cracker.py
@@ -26,23 +26,23 @@ import sys
 # -------------------------------------------------------------------------------------
 
 if os.geteuid() != 0:
-   print "Please run this python script as root..."
+   print("Please run this python script as root...")
    exit(True)
 
 if len(sys.argv) < 2:
-   print "Use the command python zip-cracker.py secure.zip..."
+   print("Use the command python zip-cracker.py secure.zip...")
    exit(True)
 
 filename = sys.argv[1]
 
 if os.path.exists(filename) == 0:
-   print "File " + filename + " was not found, did you spell it correctly?..."
+   print("File " + filename + " was not found, did you spell it correctly?...")
    exit(True)
 
 filextends = filename[-3:]
 
 if filextends != "zip":
-   print "Incorrect file format...."
+   print("Incorrect file format....")
    exit (True)
 
 # -------------------------------------------------------------------------------------
@@ -55,15 +55,15 @@ if filextends != "zip":
 
 def header ():
    os.system("clear")
-   print " ________ ____     ____ ____      _    ____ _  _______ ____   "
-   print "|__  /_ _|  _ \   / ___|  _ \    / \  / ___| |/ / ____|  _ \  "
-   print "  / / | || |_) | | |   | |_) |  / _ \| |   | ' /|  _| | |_) | "
-   print " / /_ | ||  __/  | |___|  _ <  / ___ \ |___| . \| |___|  _ <  "
-   print "/____|___|_|      \____|_| \_\/_/   \_\____|_|\_\_____|_| \_\ "
-   print "                                                              "
-   print "    BY TERENCE BROADBENT BSc CYBER SECURITY (FIRST CLASS)     "
-   print "                                                              "
-   print "FILENAME: " + filename + ".\n"
+   print(" ________ ____     ____ ____      _    ____ _  _______ ____   ")
+   print("|__  /_ _|  _ \   / ___|  _ \    / \  / ___| |/ / ____|  _ \  ")
+   print("  / / | || |_) | | |   | |_) |  / _ \| |   | ' /|  _| | |_) | ")
+   print(" / /_ | ||  __/  | |___|  _ <  / ___ \ |___| . \| |___|  _ <  ")
+   print("/____|___|_|      \____|_| \_\/_/   \_\____|_|\_\_____|_| \_\ ")
+   print("                                                              ")
+   print("    BY TERENCE BROADBENT BSc CYBER SECURITY (FIRST CLASS)     ")
+   print("                                                              ")
+   print("FILENAME: " + filename + ".\n")
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
@@ -81,11 +81,11 @@ for check in checklist:
    cmd = "locate -i " + check + " > /dev/null"
    checked = os.system(cmd)
    if checked != 0:
-      print "I cannot find " + check + "..."
+      print("I cannot find " + check + "...")
       installed = False
 
 if installed == False:
-   print "\nInstall missing dependencies before you begin...\n"
+   print("\nInstall missing dependencies before you begin...\n")
    exit (True)
 
 # -------------------------------------------------------------------------------------
@@ -104,12 +104,12 @@ menu['[4].']="Exit."
 
 while True: 
    header()
-   options=menu.keys()
+   options=list(menu.keys())
    options.sort()
    for entry in options: 
-      print entry, menu[entry]
-   selection=raw_input("\nPlease Select: ")
-   print ""
+      print(entry, menu[entry])
+   selection=input("\nPlease Select: ")
+   print("")
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
@@ -120,21 +120,21 @@ while True:
 # -------------------------------------------------------------------------------------
 
    if selection =='1':
-      print "Crack Selected    : Dictionary attack."
+      print("Crack Selected    : Dictionary attack.")
       dictionary = "/usr/share/wordlists/rockyou.txt"		# change as you require.
       if os.path.isfile(dictionary):
-         print "Crack Dictionary  : " + dictionary + "."
+         print("Crack Dictionary  : " + dictionary + ".")
       else:
-         print "System Error      : Dictionary not found."
+         print("System Error      : Dictionary not found.")
          exit(True)
-      print "Crack Status      : Cracking file."
+      print("Crack Status      : Cracking file.")
       os.system("fcrackzip -v -D -u -p " + dictionary + " '" + filename + "' > Answer.txt")
       os.system("awk '/pw ==/{print $NF}' Answer.txt > Password.txt")
       password = open("Password.txt").readline().rstrip()
       if password == "":	
-         print "Crack Status      : Dictionary exhausted.\n"
+         print("Crack Status      : Dictionary exhausted.\n")
       else:      
-         print "Cracked Password  : " + password + ".\n"
+         print("Cracked Password  : " + password + ".\n")
       os.remove('Answer.txt')
       os.remove('Password.txt')
       exit (False)
@@ -148,20 +148,20 @@ while True:
 # -------------------------------------------------------------------------------------
 
    elif selection == '2':
-      print "Crack Selected    : Hash attack."
-      print "Crack Status      : Creating hash."
+      print("Crack Selected    : Hash attack.")
+      print("Crack Status      : Creating hash.")
       os.system("zip2john '" + filename + "' > hash.txt 2>&1")
       os.system("sed 1d hash.txt > hash2.txt")
       os.remove('hash.txt')
-      print "Crack Status      : Comparing hash values."
+      print("Crack Status      : Comparing hash values.")
       os.system("john hash2.txt --pot=pass.txt > dump.txt 2>&1")
       password = open("pass.txt").readline()
       a,b =  password.split(':')
       b = b.replace('\n','')
       if password == "":
-         print "Crack Status      : Hash crack exhausted.\n"
+         print("Crack Status      : Hash crack exhausted.\n")
       else:
-         print "Cracked Password  : " + b + "."
+         print("Cracked Password  : " + b + ".")
       os.remove('hash2.txt')
       os.remove('dump.txt')
       os.remove('pass.txt')
@@ -176,24 +176,24 @@ while True:
 # -------------------------------------------------------------------------------------
 
    elif selection == '3':
-      print "Crack Selected    : Brute force attack (1-8 characters)."
-      print "Crack Status      : Conducting numeric attack first."
+      print("Crack Selected    : Brute force attack (1-8 characters).")
+      print("Crack Status      : Conducting numeric attack first.")
       os.system("fcrackzip -c 1 -m zip1 -l 1-8 -u '" + filename + "' > Answer.txt")
       os.system("awk '/pw ==/{print $NF}' Answer.txt > Password.txt")
       password = open("Password.txt").readline().rstrip()
       if password == "":	
-         print "Crack Status      : Numeric bruteforce exhausted."
+         print("Crack Status      : Numeric bruteforce exhausted.")
       else:      
-         print "Cracked Password  : " + password + ".\n"
+         print("Cracked Password  : " + password + ".\n")
       if password == "":
-         print "Crack Status      : Now trying alphanumeric."
+         print("Crack Status      : Now trying alphanumeric.")
          os.system("fcrackzip -m zip1 -l 1-8 -u '" + filename + "' > Answer.txt")
          os.system("awk '/pw ==/{print $NF}' Answer.txt > Password.txt")
          password = open("Password.txt").readline().rstrip()
          if password == "":	
-            print "Crack Status      : Alphanumeric bruteforce exhausted.\n"
+            print("Crack Status      : Alphanumeric bruteforce exhausted.\n")
          else:      
-            print "Cracked Password  : " + password + ".\n"
+            print("Cracked Password  : " + password + ".\n")
       os.remove('Answer.txt')
       os.remove('Password.txt')
       exit (False)
